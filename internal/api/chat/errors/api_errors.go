c5430ac9883c7f2be4f4595d49569ca78c5f6563
Dmitriy <dimkkononov@gmail.com>
2024-11-03 17:22:51 +0300

    small fixes

--- internal/api/chat/errors/api_errors.go
+++ internal/api/chat/errors/api_errors.go
@@ -3,6 +3,6 @@ package errors
 import "fmt"
 
 var (
-	ErrDescChatIsNil    = fmt.Errorf("desc chat is nil")
-	ErrDescMessageIsNil = fmt.Errorf("desc message is nil")
+	ErrDescChatIsNil    = fmt.Errorf("desc chat is nil")    // ErrDescChatIsNil grpc запрос с чатом nil
+	ErrDescMessageIsNil = fmt.Errorf("desc message is nil") // ErrDescMessageIsNil grpc запрос с сообщением nil
 )
