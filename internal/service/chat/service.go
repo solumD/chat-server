8b2187fb130cf62a8a2cd2cc7d7fdcce26b156f5
Dmitriy <dimkkononov@gmail.com>
2025-03-01 17:49:22 +0300

    fix send message

--- internal/service/chat/service.go
+++ internal/service/chat/service.go
@@ -187,12 +187,29 @@ func (s *srv) ConnectChat(ctx context.Context, chatID int64, username string,
 
 }
 
-// SendMessage отправляет запрос в репо слой на отправку (сохранение) сообщения
+// SendMessage сохраняет сообщение в репо и отправляет его в чат через stream
 func (s *srv) SendMessage(ctx context.Context, message *model.Message) (*emptypb.Empty, error) {
+	// проверяем, существует ли канал для сообщений чата
+	s.mu.RLock()
+	chatMsgChan, exist := s.msgChans[message.ChatID]
+	s.mu.RUnlock()
+
+	if !exist {
+		return nil, fmt.Errorf("chat's %d connection not exist. connect to create it", message.ChatID)
+	}
+
+	// проверяем, подключен ли пользователь к чату
+	s.mu.RLock()
+	_, exist = s.chatStreams[message.ChatID][message.From]
+	s.mu.RUnlock()
+
+	if !exist {
+		return nil, fmt.Errorf("user %s is not connected to chat. connect to chat", message.From)
+	}
+
 	if len(message.From) == 0 {
 		return nil, fmt.Errorf("from can't be empty")
 	}
-
 	if len(message.Text) == 0 {
 		return nil, fmt.Errorf("message's text can't be empty")
 	}
@@ -209,6 +226,11 @@ func (s *srv) SendMessage(ctx context.Context, message *model.Message) (*emptypb
 
 	if err != nil {
 		return nil, err
+	}
+
+	chatMsgChan <- &chat_v1.Message{
+		From: message.From,
+		Text: message.Text,
 	}
 
 	return &emptypb.Empty{}, nil
